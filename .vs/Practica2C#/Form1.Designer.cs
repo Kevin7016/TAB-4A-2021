3ead25b2a3adf4735e4a1a7b288868f1b291fa27
Kevin7016 <7016@itescam.edu.mx>
2021-04-26 15:30:52 -0500

    Enter

---
+++ .vs/Practica2C#/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.principalarea = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.principalarea)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // principalarea
+            // 
+            this.principalarea.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.principalarea.Location = new System.Drawing.Point(0, 0);
+            this.principalarea.Margin = new System.Windows.Forms.Padding(2);
+            this.principalarea.Name = "principalarea";
+            this.principalarea.Size = new System.Drawing.Size(430, 422);
+            this.principalarea.TabIndex = 0;
+            this.principalarea.TabStop = false;
+            this.principalarea.Click += new System.EventHandler(this.areaTrabajo_Click);
+            this.principalarea.Paint += new System.Windows.Forms.PaintEventHandler(this.areaTrabajo_Paint);
+            this.principalarea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.areaTrabajo_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(95, 20);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(96, 75);
+            this.panel1.TabIndex = 1;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(-2, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Comando1 Reg(C)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(297, 164);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(52, 57);
+            this.panel2.TabIndex = 2;
+            this.panel2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.panel2_Scroll);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseCaptureChanged += new System.EventHandler(this.panel2_MouseCaptureChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(195, 20);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(96, 75);
+            this.panel3.TabIndex = 3;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(-2, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Comando 2Mov(X)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 419);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.principalarea);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.principalarea)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox principalarea;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ .vs/Practica2C#/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica2
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        int Xder, Xizq;
+        int Yabj, Yarb;
+        bool max;
+        bool mover;
+        bool mini;
+        public Form1()
+        {
+            InitializeComponent();
+            Xder = 974;
+            Yabj = 820;
+            max = false;
+            mini = false;
+        }
+
+        private void areaTrabajo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillRectangle(Brushes.Red,10,110,200,300);
+            g.DrawRectangle(Pens.White, 10, 110, 200, 300);
+            g.FillRectangle(Brushes.White, 220, 110, 200, 300);
+            g.DrawRectangle(Pens.Black, 220, 110, 200, 300);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics s = e.Graphics;
+            s.FillEllipse(Brushes.Black, 0, 0, 50, 50);
+            s.DrawRectangle(Pens.White, 0, 0, 50, 50);
+        }
+
+        private void panel2_MouseCaptureChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void areaTrabajo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mover == true)
+            {
+                Point mouseMoveLocation = new Point(e.X, e.Y);
+                x = e.X;
+                y = e.Y;
+
+                colision();
+                panel2.Location = new System.Drawing.Point(x, y);
+                if (x <= 180 && y <= 422)
+                {
+                    max = true;
+                }
+                if (max)
+                {
+                    Xder = 158;
+                    Yabj = 343;
+                }
+                if(x >= 10 && y >= 100)
+                {
+                    mini = true;
+                }
+                if(mini)
+                {
+                    Xizq = 10;
+                    Yarb = 100;
+                }
+                colision();
+            }
+        }
+        public void colision()
+        {
+
+            if (x >= Xder)
+            {
+                x = Xder;
+            }
+            if (y >= Yabj)
+            {
+                y = Yabj;
+            }
+            if(x <= Xizq)
+            {
+                x = Xizq;
+            }
+            if(y <= Yarb)
+            {
+                y = Yarb;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            mover = true;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle r = new Rectangle(10, 1, 75, 75);
+            Graphics f = e.Graphics;
+            f.FillEllipse(Brushes.Green, r);
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            mover = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar.ToString());
+            if (e.KeyChar == 'x' || e.KeyChar == 'X')
+            {
+                mover = true;
+            }
+            if (e.KeyChar == 'c' || e.KeyChar == 'C')
+            {
+                x = 296;
+                y = 254;
+                panel2.Location = new System.Drawing.Point(x, y);
+                mover = false;
+                Xder = 574;
+                Yabj = 520;
+                max = false;
+                mini = false;
+            }
+        }
+
+        private void areaTrabajo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            x = 296;
+            y = 254;
+            panel2.Location = new System.Drawing.Point(x, y);
+            mover = false;
+            Xder = 574;
+            Yabj = 520;
+            max = false;
+            mini = false;
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            x = 296;
+            y = 254;
+            panel2.Location = new System.Drawing.Point(x, y);
+            mover = false;
+            Xder = 574;
+            Yabj = 520;
+            max = false;
+            mini = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle r = new Rectangle(10, 1, 75, 75);
+            Graphics f = e.Graphics;
+            f.FillEllipse(Brushes.Green, r);
+        }
+
+    }
+}
