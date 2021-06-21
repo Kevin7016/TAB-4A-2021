83f53f7bfeb1dfd944d861660ba9ffc5a8e4ce73
Kevin7016 <7016@itescam.edu.mx>
2021-06-21 12:05:56 -0500

    Enter

---
+++ ejercicios2p/InterfaceCliente.java
@@ -0,0 +1,48 @@
+package ejercicios2p;
+import java.awt.*;
+import javax.swing.*;
+
+public class InterfaceCliente extends JFrame {
+
+    public InterfaceCliente() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new GridLayout());
+
+        contPanel = new JPanel();
+        imgFondo = new JLabel();
+        btnAceptar = new JButton();
+
+        contPanel.setLayout(null);
+
+        imgFondo.setText("Hola Mundo");
+        contPanel.add(imgFondo);
+        imgFondo.setBounds(70, 30, 180, 128);
+
+        btnAceptar.setText("Aceptar");
+        contPanel.add(btnAceptar);
+        btnAceptar.setBounds(170, 120, 120, 23);
+
+        getContentPane().add(contPanel);
+
+        // Personalizacion de la ventana
+        this.setSize(350, 500);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        InterfaceCliente user = new InterfaceCliente();
+    }
+
+    // Variable
+    JPanel contPanel;
+    JLabel imgFondo;
+    JButton btnAceptar;
+    // End Variable
+
+}
